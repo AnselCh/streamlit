b0b070253188b2dbcf23616fd39af73750f69715
AnselCh <anannannan0102@gmail.com>
2023-01-03 14:43:56 +0800

    ann

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+########################
+
+st.title('Streamlit Applications')
+st.markdown('''
+ ### AgeCaculator
+ > ######  用生日計算出星座
+ > ######  並結合bs4爬蟲，顯示星座運勢
+''')
+
+st.markdown('''
+ ### RemobeBackground
+ > ######  使用rembg套件網頁版去背app
+''')
